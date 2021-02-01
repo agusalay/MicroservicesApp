38f0db859f6f69fa5f1fd59537ca4e8426aaa05e
agus umriadi <agus_umriadi@ymail.com>
2021-02-01 21:45:31 +0700

    Add project files.

---
+++ src/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,56 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection) {
+            bool existProduct = productCollection.Find(p => true).Any();
+
+            if (existProduct)
+                return;
+
+            productCollection.InsertManyAsync(GetPreconfiguredProduct());
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProduct()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Iphone X",
+                    Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque cursus consectetur dolor nec luctus. Sed aliquam, libero id aliquet suscipit, orci diam semper tortor, quis convallis neque metus in tellus",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                    ImageFie = "product-1.png",
+                    Price = 950,
+                    Category = "Smart Phone"
+                },
+                 new Product()
+                {
+                    Name = "Samsung X",
+                    Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque cursus consectetur dolor nec luctus. Sed aliquam, libero id aliquet suscipit, orci diam semper tortor, quis convallis neque metus in tellus",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                    ImageFie = "product-2.png",
+                    Price = 1000,
+                    Category = "Smart Phone"
+                },
+                  new Product()
+                {
+                    Name = "Xiaomi X",
+                    Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque cursus consectetur dolor nec luctus. Sed aliquam, libero id aliquet suscipit, orci diam semper tortor, quis convallis neque metus in tellus",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                    ImageFie = "product-1.png",
+                    Price = 500,
+                    Category = "Dumb Phone"
+                }
+
+            };
+        }
+    }
+}
---
+++ src/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,72 @@
+﻿using Catalog.API.Data.Interfaces;
+using Catalog.API.Entities;
+using Catalog.API.Repositories.Interfaces;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContexts _contexts;
+
+        public ProductRepository(ICatalogContexts contexts)
+        {
+            _contexts = contexts;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _contexts.Products.Find(p => true).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            FilterDefinition <Product> filter = Builders<Product>.Filter.ElemMatch(p => p.Category, categoryName);
+
+            return await _contexts
+                .Products
+                .Find(filter)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.ElemMatch(p => p.Name, name);
+
+            return await _contexts
+                .Products
+                .Find(filter)
+                .ToListAsync();
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _contexts.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task Create(Product product)
+        {
+            await _contexts.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> Delete(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+
+            var deleteResult = await _contexts.Products.DeleteOneAsync(filter);
+
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
+
+        public async Task<bool> Update(Product product)
+        {
+            var updateResult = await _contexts.Products.ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+        }
+    }
+}
