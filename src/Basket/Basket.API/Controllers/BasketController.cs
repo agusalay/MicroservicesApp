27ea9c8609b8f7700a0a1e5ba0e94c8a481be953
agus umriadi <agus_umriadi@ymail.com>
2021-02-02 14:49:04 +0700

    Add RabbitMQ basket producer

--- src/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,5 +1,9 @@
-﻿using Basket.API.Entities;
+﻿using AutoMapper;
+using Basket.API.Entities;
 using Basket.API.Repositories.Interfaces;
+using EventBusRabbitMQ.Common;
+using EventBusRabbitMQ.Events;
+using EventBusRabbitMQ.Producer;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -15,10 +19,14 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly EventBusRabbitMQProducer _eventBus;
 
-        public BasketController(IBasketRepository repository)
+        public BasketController(IBasketRepository repository, IMapper mapper, EventBusRabbitMQProducer eventBus)
         {
             _repository = repository;
+            _mapper = mapper;
+            _eventBus = eventBus;
         }
 
         [HttpGet]
@@ -48,6 +56,39 @@ namespace Basket.API.Controllers
         public async Task<ActionResult<bool>> DeleteBasketCart(string userName)
         {
             return Ok(await _repository.DeleteBasket(userName));
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.Accepted)]
+        public async Task<ActionResult<bool>> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            var basket = await _repository.GetBasket(basketCheckout.UserName);
+
+            if (basket == null)
+                return BadRequest();
+
+            var basketRemoved = await _repository.DeleteBasket(basket.UserName);
+
+            if (!basketRemoved)
+                return BadRequest();
+
+            var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            eventMessage.RequestId = Guid.NewGuid();
+            eventMessage.TotalPrice = basket.TotalPrice;
+
+            try
+            {
+                _eventBus.PublishBasketCheckout(EventBusConstants.BasketCheckoutQueue, eventMessage);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return Accepted();
+
         }
 
     }
