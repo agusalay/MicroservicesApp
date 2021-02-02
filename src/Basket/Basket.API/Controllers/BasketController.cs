e40ad73d54371b1f5a22029e13991f4b9fa8822f
agus umriadi <agus_umriadi@ymail.com>
2021-02-02 09:52:06 +0700

    Add basket API and Redis

---
+++ src/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,54 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<BasketCart>> GetBasketCart(string userName)
+        {
+            var basketcart = await _repository.GetBasket(userName);
+
+            if (basketcart == null)
+                return new BasketCart(userName);
+
+            return Ok(basketcart);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<BasketCart>> UpdateBasketCart([FromBody] BasketCart basketCart)
+        {
+            var basketcart = await _repository.UpdateBasket(basketCart);
+
+            return Ok(basketcart);
+        }
+
+        [HttpDelete("{userName}")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> DeleteBasketCart(string userName)
+        {
+            return Ok(await _repository.DeleteBasket(userName));
+        }
+
+    }
+}
